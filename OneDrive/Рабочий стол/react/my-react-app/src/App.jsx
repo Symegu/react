62e24fd0c73b30ef0663818a0c88e1eac65bc6b1
Symegu <89914120+Symegu@users.noreply.github.com>
2022-08-28 19:39:25 +0300

    1 lesson

---
+++ OneDrive/Рабочий стол/react/my-react-app/src/App.jsx
@@ -0,0 +1,12 @@
+// import { useState } from 'react';
+import { MessageClass } from './class-components/Message';
+import './index.css'
+
+export const App = () => {
+  return (
+    <div>
+      <MessageClass messageList={['App_props-1 ', 'App_props-2 ']} />
+    </div>
+  );
+}
+
