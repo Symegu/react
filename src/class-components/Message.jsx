72a76cd75edd91d3ee3cc225837eb7eb3b80f3a0
Symegu <89914120+Symegu@users.noreply.github.com>
2022-08-28 20:14:37 +0300

    1 lesson hw

---
+++ src/class-components/Message.jsx
@@ -0,0 +1,20 @@
+import { MessageFunc } from '../func-components/Message'
+import { Component } from 'react'
+
+export class MessageClass extends Component {
+  state = {
+    messageList: [
+        'Class_props1 ',
+        'Class_props2 '
+    ]
+  }
+
+  render() {
+    return (
+      <div style={{backgroundColor: 'grey'}}>
+        <h3>Message props from App: {this.props.messageList}</h3>
+        <MessageFunc messageList={this.state.messageList} />
+      </div>
+    );
+  }
+}
